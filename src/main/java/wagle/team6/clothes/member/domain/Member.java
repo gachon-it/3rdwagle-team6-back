b9797660d194716b93d55cefd3f350a67093d300
ohu-star <audtj0703@naver.com>
2025-02-21 22:50:02 +0900

    [feat] 엔티티 생성

--- src/main/java/wagle/team6/clothes/member/domain/Member.java
+++ src/main/java/wagle/team6/clothes/member/domain/Member.java
@@ -1,4 +1,21 @@
 package wagle.team6.clothes.member.domain;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    @Embedded
+    private Email email;
+
+    @Embedded
+    private Password password;
+
 }
