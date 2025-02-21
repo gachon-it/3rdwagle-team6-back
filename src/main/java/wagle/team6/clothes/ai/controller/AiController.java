24590add1e21667a2a020ac201e385b580fb8060
passionryu <rsy1225@naver.com>
2025-02-22 03:56:48 +0900

    [feat] 옷 추천 api 구현 완료

--- src/main/java/wagle/team6/clothes/ai/controller/AiController.java
+++ src/main/java/wagle/team6/clothes/ai/controller/AiController.java
@@ -3,11 +3,9 @@ package wagle.team6.clothes.ai.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wagle.team6.clothes.ai.dto.Request.ChattingRequest;
+import wagle.team6.clothes.ai.dto.Response.ClothesResponse;
 import wagle.team6.clothes.ai.service.AiService;
 
 @Slf4j
@@ -18,23 +16,42 @@ public class AiController {
 
     private final AiService aiService;
 
-    /* 채팅 API */
+    /**
+     * 채팅 API
+     * @param chattingRequest
+     * @return
+     */
     @PostMapping("/chats")
     public ResponseEntity<String> chat(@RequestBody ChattingRequest chattingRequest){
 
+        /* 채팅 메시지 반환 */
         String aiResponse = aiService.chat(chattingRequest);
 
         return ResponseEntity.ok(aiResponse);
     }
 
-    /* 추천 API */
-    @PostMapping("/recommends")
-    public ResponseEntity<String> recommend(){
+    /**
+     * 추천 API
+     * @param userId
+     * @return
+     */
+    @PostMapping("/recommends/{userId}")
+    public ResponseEntity<ClothesResponse> recommend(@PathVariable("userId") Long userId){
 
-        return null;
+        /* 채팅을 기반으로 상의 추천 */
+        String topColor = aiService.recommend(userId);
+
+        /* 상의 색 추천 */
+        log.info("recommend top color: {}", topColor);
+
+        /* 색이 매칭 되는 하의 추천 */
+        String bottomColor = aiService.match(topColor);
+        log.info("recommend bottom color: {}", bottomColor);
+
+        /* ClothesResponse 객체 생성 */
+        ClothesResponse clothesResponse = new ClothesResponse(topColor, bottomColor);
+
+        return ResponseEntity.ok(clothesResponse);
     }
-
-
-
 
 }
--- src/main/java/wagle/team6/clothes/ai/service/AiService.java
+++ src/main/java/wagle/team6/clothes/ai/service/AiService.java
@@ -1,11 +1,18 @@
 package wagle.team6.clothes.ai.service;
 
 import wagle.team6.clothes.ai.dto.Request.ChattingRequest;
+import wagle.team6.clothes.ai.dto.Response.ClothesResponse;
 
 public interface AiService {
 
     /* 채팅 메서드 */
     String chat(ChattingRequest chattingRequest);
+
+    /* 의상 추천 메서드 */
+    String recommend(Long userId);
+
+    /* 의상 매칭 메서드 */
+    String match(String topColor);
 
 
 }
