b66a78b598baef05f5bb140bf5330eda1f51bd64
yeowon0411 <dudnjsdl11@naver.com>
2025-02-22 01:28:45 +0900

    [feat] 회원가입,로그인 구현

--- src/main/java/wagle/team6/clothes/member/controller/MemberController.java
+++ src/main/java/wagle/team6/clothes/member/controller/MemberController.java
@@ -1,4 +1,34 @@
 package wagle.team6.clothes.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wagle.team6.clothes.member.domain.Member;
+import wagle.team6.clothes.member.dto.MemberRequestDTO;
+import wagle.team6.clothes.member.service.MemberService;
+
+@Controller
+@RestController
+@RequestMapping("/member")
 public class MemberController {
+
+
+    private final MemberService memberService;
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/sign-up")
+    public Long signUp(@RequestBody MemberRequestDTO request) {
+        return memberService.signUp(request);
+    }
+
+
+    @PostMapping("/login")
+    public Member login(@RequestBody MemberRequestDTO request) {
+        return memberService.login(request);
+    }
+
 }
--- src/main/java/wagle/team6/clothes/member/repository/MemberRepository.java
+++ src/main/java/wagle/team6/clothes/member/repository/MemberRepository.java
@@ -1,4 +1,17 @@
 package wagle.team6.clothes.member.repository;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import wagle.team6.clothes.member.domain.Email;
+import wagle.team6.clothes.member.domain.Member;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmailAndPassword(String email,String password);
+
+
 }
